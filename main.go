043e28d319486e4104a5b37d64af5b482bc2d15b
dimasm93 <dimas.maryanto@tabeldata.com>
2022-09-14 14:59:43 +0700

    go fiber web framework and viper for external configuration

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/spf13/viper"
+	"log"
+)
 
 func main() {
-	fmt.Println("Golang apps")
+	// read configuration
+	viper.SetConfigType("yaml")
+	viper.SetConfigFile("env.yaml")
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// initialize web application
+	app := fiber.New()
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	app_port := viper.GetString("APP_PORT")
+	log.Fatal(app.Listen(":" + app_port))
 }
