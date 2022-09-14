7596024868b11ecf675e5427c7ef100fd2849330
dimasm93 <dimas.maryanto@tabeldata.com>
2022-09-14 18:39:18 +0700

    update route

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/requestid"
 	"github.com/spf13/viper"
 	"log"
 	"os"
@@ -18,6 +20,20 @@ func main() {
 
 	// initialize web application
 	app := fiber.New()
+
+	// enable logging file
+	file, err := os.OpenFile("./logs/application.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer file.Close()
+
+	app.Use(requestid.New())
+	app.Use(logger.New(logger.Config{
+		Format: "${pid} ${locals:requestid} ${status} - ${method} ${path}\n",
+		Output: file,
+	}))
+
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
